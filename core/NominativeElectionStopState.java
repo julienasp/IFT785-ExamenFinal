f0e61202a6dd37b8daa3f3cd662ab3d6855ebd69
julienasp <julien.aspirot@usherbrooke.ca>
2016-04-28 16:48:49 -0400

    state done for nominativeElection

--- core/NominativeElectionStopState.java
+++ core/NominativeElectionStopState.java
@@ -8,41 +8,41 @@ package core;
  *
  * @author JUASP-G73-Android
  */
-public class NominativeElectionStopState implements IReferendumState {
+public class NominativeElectionStopState implements INominativeElectionState {
     @Override
     public int getStateCode() {
         return Election.TERMINE;	
     }
     
      @Override
-    public Object getReferendumResult(Referendum r) {
-        Log.log("ReferendumStopState-getReferendumResult(): invalide state, the Referendum need to be stop first.");
+    public Object getNominativeElectionResult(NominativeElection ne) {
+        Log.log("NominativeElectionStopState-getNominativeElectionResult(): invalide state, the Referendum need to be stop first.");
         
         //Use of the Strategy pattern inside the state pattern
-        return r.getVotingSystemStandard().processingResult(r);
+        return ne.getVotingSystemStandard().processingResult(ne);
     }
 
     @Override
-    public void doVoting(Referendum r){
-        Log.log("ReferendumStopState-doVoting(): invalide state, the Referendum need to be ongoing to vote.");
+    public void doVoting(NominativeElection ne){
+        Log.log("NominativeElectionStopState-doVoting(): invalide state, the Referendum need to be ongoing to vote.");
     }
     
     @Override
-    public void doStateAction(Referendum r) {
-        if(r.getState().getStateCode() == Election.OUVERT){
-            Log.log("ReferendumStopState-doStateAction(): doing action relative to the end of the Referendum");
+    public void doStateAction(NominativeElection ne) {
+        if(ne.getState().getStateCode() == Election.OUVERT){
+            Log.log("NominativeElectionStopState-doStateAction(): doing action relative to the end of the Referendum");
             
             //We set the new State
-            r.setState(this);
+            ne.setState(this);
             
             //We change the state on the Subject, so he can notify all the observers
-            r.getSubject().setState(this.getStateCode()); 
+            ne.getSubject().setState(this.getStateCode()); 
             
             //We proceed
-            r.depouiller();
+            ne.depouiller();
         }
         else{
-            Log.log("ReferendumStopState-doStateAction(): invalide state, the Referendum need to be started first.");  
+            Log.log("NominativeElectionStopState-doStateAction(): invalide state, the Referendum need to be started first.");  
         }
     }
 }
